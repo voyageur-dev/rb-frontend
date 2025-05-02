219b37ad68173c31b56e2d21d0eb57c54cec6466
kcwong395 <kcwong395@gmail.com>
2025-05-02 12:50:27 -0400

    minor fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GATEWAY_BASEURL: process.env.NEXT_PUBLIC_GATEWAY_BASEURL,
   },
   typescript: {
